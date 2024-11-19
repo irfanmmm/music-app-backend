e26316fedfe1d543f230f32601759e80800b481c
irfan <irfan@nextgbl.com>
2024-11-19 10:45:46 +0530

    mongo db connected

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const { MongoClient } = require("mongodb");
 
 const DataBase = async () => {
   const url =
-    "mongodb://localhost:27017/";
+    "mongodb+srv://koyarabigh890:Hv8xKk8W4h7x89f1@cluster0.aothz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
   const dbName = "irfan";
   let client = await MongoClient.connect(url, {
     useNewUrlParser: true,
